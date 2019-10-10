3f54b4b3364c099b11c6ec865ee2ac5dfc7c44fb
Suylo <spoownz@yahoo.com>
2019-10-10 19:38:14 +0200

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.chart').easyPieChart({
+        scaleColor: "transparent",
+        lineWidth: 20,
+        lineCap: 'butt',
+        barColor: '#6F9FD8',
+        trackColor:	"transparent",
+        size: 160,
+        animate: 500
+    });
+});
